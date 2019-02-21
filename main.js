a5b40b6a7b86694b68de2cd8ad2b0c6300d9acd3
Olivier <olivier_boniteau@yahoo.fr>
2019-02-21 23:27:28 +0100

    misc

--- main.js
+++ main.js
@@ -5420,16 +5420,12 @@ var SidenavcontainerComponent = /** @class */ (function () {
 "use strict";
 __webpack_require__.r(__webpack_exports__);
 /* harmony export (binding) */ __webpack_require__.d(__webpack_exports__, "environment", function() { return environment; });
-// The file contents for the current environment will overwrite these during build.
-// The build system defaults to the dev environment which uses `environment.ts`, but if you do
-// `ng build --env=prod` then `environment.prod.ts` will be used instead.
-// The list of which env maps to which file can be found in `.angular-cli.json`.
 var environment = {
-    production: false,
+    production: true,
     defaultlocale: 'en',
-    locale: 'fr-FR',
-    server: '',
-    log: 'debug',
+    locale: 'en-UK',
+    server: '/mobilecms-demo/',
+    log: 'error',
     public: 'public'
 };
 
